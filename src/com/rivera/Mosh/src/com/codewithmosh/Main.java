7786466f51f335e6bc32f3099f8662594ff9d430
GrethelRivera <grethelr91@gmail.com>
2022-06-23 19:00:03 -0400

    clean up repo

--- src/com/rivera/Mosh/src/com/codewithmosh/Main.java
+++
@@ -1,8 +0,0 @@
-package com.codewithmosh;
-
-public class Main {
-
-    public static void main(String[] args) {   //parameters
-        System.out.println("Hello World"); // Type code here. // Text needs double quotes
-    }
-}
